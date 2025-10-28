dee7f1f5670dd63d240e183ae874177296dbbb32
sukarxn <thecodersmailbox@gmail.com>
2025-10-28 17:12:20 +0530

    9: added PATH search for executable command files, recheck

--- app/main.ts
+++ app/main.ts
@@ -35,7 +35,7 @@ const handleCommand  = (input: string) => {
   if (command === "exit") {
     if(args.length > 1) {
       rl.write('exit: too many arguments\n');
-      return true;
+      return;
     }
     const exitCode = parseInt(args[0] || "0", 10);
     rl.close();
@@ -44,13 +44,13 @@ const handleCommand  = (input: string) => {
 
   if(command === "echo") {
     rl.write(args.join(" ") + "\n");
-    return true;
+    return;
   }
 
   if(command === "type") {
     if(args.length > 1) {
       rl.write('type: too many arguments\n');
-      return true;
+      return;
     }
 
     // if(["type", "echo", "exit"].includes(args[0])) {
@@ -64,21 +64,21 @@ const handleCommand  = (input: string) => {
 
     if(["type", "echo", "exit"].includes(args[0])) {
       rl.write(`${args[0]} is a shell builtin\n`);
-      return true;
+      return;
 
     } else if (checkPath(args[0])) {
       rl.write(`${args[0]} is ${checkPath(args[0])}\n`);
-      return true;
+      return;
 
     } else {
       rl.write(`${args[0]}: not found\n`);
-      return false;
+      return;
     }
   }
 
   // if command is not recognized in any of the above cases
   rl.write(command + ": command not found\n");
-  return false; 
+  return; 
   }
 
 // TODO: Uncomment the code below to pass the first stage
