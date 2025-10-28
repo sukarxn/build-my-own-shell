31ad4d098b26c4d61447f723736c087cf23679cb
sukarxn <thecodersmailbox@gmail.com>
2025-10-28 10:56:03 +0530

    6: test exit with args

--- app/main.ts
+++ app/main.ts
@@ -8,12 +8,20 @@ const rl = createInterface({
 // TODO: Uncomment the code below to pass the first stage
 const myShell = () => {
   rl.question("$ ", (answer) => {
-    if (answer === "exit") {
+    const parts = answer.trim().split(" ");
+    const command = parts[0];
+    const args = parts.slice(1);
+
+    if (command === "exit") {
+      const exitCode = args[0] ? parseInt(args[0]) : 0;
+      exitCode == 0 ? rl.write(`Exited with success: ${exitCode}\n`) : rl.write(`Exited with error code: ${exitCode}\n`);
       rl.close();
       return;
     }
+
   rl.write(answer + ": command not found\n");
   myShell();
+
   });
 }
 
